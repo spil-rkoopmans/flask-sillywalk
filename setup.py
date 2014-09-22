ad39cfdb8b65f741b3625af39879cf3b75096387
Remco Koopmans <Remco.Koopmans@spilgames.com>
2014-09-22 19:29:14 +0200

    Renamed flask Swagger to flask_sillywalk for our own pypi server

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ from setuptools import setup
 
 setup(
     name='Flask-Swagger',
-    version='1.0',
-    url='https://github.com/hobbeswalsh/flask-swagger',
+    version='1.2',
+    url='https://github.com/spil-rkoopmans/flask-sillywalk',
     license='BSD',
     author='Robin Walsh & Harvey Rogers',
     author_email='rob.walsh@gmail.com',
     description='So you want to implement an auto-documenting API?',
     long_description=__doc__,
-    py_modules=['flask_swagger'],
+    py_modules=['flask_sillywalk'],
     zip_safe=False,
     include_package_data=True,
     platforms='any',
@@ -29,7 +29,10 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Spilgames :: Internal'
     ]
 )
